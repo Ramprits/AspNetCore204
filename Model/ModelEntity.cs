5d236657354e82950a19126b4a0609597081f190
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-12-13 19:23:02 +0530

    campaign added

--- Model/ModelEntity.cs
+++ Model/ModelEntity.cs
@@ -3,6 +3,6 @@ using System;
 namespace AspNetCoreApplication.Model {
     public class ModelEntity {
         public byte[] RowVersion { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     }
 }
